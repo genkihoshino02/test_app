46d09eecd75d223b8401da00d55df08fc34c5357
genkihoshino02 <fzny726@gmail.com>
2020-08-11 10:17:10 +0900

    スライドダウン機能追加・結果表示を変更・写真の追加

--- test-app.js
+++ test-app.js
@@ -5,22 +5,37 @@ const submitBtn=document.getElementById("submit");
 // クイズのデータ
 let questions=[
     {
-        question:"What does CEO stand for?",
+        question:"Which of the following are pyrimidines?",
         answers:{
-            a:"Chief excutive officer",
-            b:"chewing excellent officer",
-            c:"No idea"
+            a:"adenine and cytosine",
+            b:"adenine and thymine",
+            c:"cytosine and thymine"
         },
-        correctAns:"a"
+        correctAns:"c",
+        picture:"./img/quiz1.jpg" ,
+        translated:"次の内ピリミジンはどれか"
     },
     {
-        question:"What does AI stand for?",
+        question:"Which of the following are purines?",
         answers:{
-            a:"Ambitious Iron",
-            b:"Artificail Intelligence",
-            c:"No idea"
+            a:"adenine and cytosine",
+            b:"adenine and guanine",
+            c:"adenine and thymine"
         },
-        correctAns:"b"
+        correctAns:"b",
+        picture:"./img/quiz2.jpg",
+        translated:"次の内プリンはどれか"
+    },
+    {
+        question:"Who is the current CEO of apple?",
+        answers:{
+            a:"Tim Cook",
+            b:"Steve Jobs",
+            c:"Jony Ive"
+        },
+        correctAns:"a",
+        picture:"./img/quiz3.jpg",
+        translated:"アップルの現在のCEOは誰か"
     }
 ];
 
@@ -32,18 +47,22 @@ function showQuestions(){
             const answers=[];
             for(letter in currentQuestion.answers){
                 answers.push(`
-                    <label>
+                    <div>
                         <input type="radio" name="question${questionNumber}" value="${letter}">
-                        ${letter}:
+                        ${letter}  :
                         ${currentQuestion.answers[letter]}
-                    </label>
+                    </div>
                 `);
             }
 
             output.push(`
-                <div class="question">
-                    ${currentQuestion.question}
+                <div class="question mt-4">
+                   Q.${questionNumber+1} :   ${currentQuestion.question}
                  </div>
+                 <div class="translated"  style="display: none;"> ${currentQuestion.translated}</div>
+                <div class="image">
+                    <img src="${currentQuestion.picture}">
+                </div>
                  <div class="answers">
                     ${answers.join('')}
                 </div>
@@ -72,8 +91,29 @@ function showResult(){
 
        
     });
-    results.innerHTML=`${numCorrect} OUT OF ${questions.length}`
+    const percentCorrect=Math.floor(100*numCorrect/questions.length);
+    let message;
+    if(percentCorrect>=80){
+        message="Excellent";
+    }else if(percentCorrect>=60){
+      message="well done";
+    }else{
+     message="Good luck";
+    }
+    results.innerHTML=`${numCorrect} OUT OF ${questions.length} is correct.<br>
+                    your correct answer rate is ${percentCorrect}   <br>
+                    ${message}`;
+    
+
+    
 }
 
 submitBtn.addEventListener("click",showResult);
 showQuestions();
+
+
+$(document).ready(function() {
+    $(".question").click(function() {
+      $(".translated").toggle("slow");
+    });
+  });
